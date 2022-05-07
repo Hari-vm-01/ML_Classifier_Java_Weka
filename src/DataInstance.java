c1856d20b2357425e70ad8a10d668b0ddcf0972f
Hari-vm-01 <hariramveeramani@gmail.com>
2022-05-07 13:33:18 -0700

    Initial Commit
    Capturing & Labelling Training Samples using Serializable Module
    
    With Optional concatenation of Date and Timestamp

---
+++ src/DataInstance.java
@@ -0,0 +1,24 @@
+import java.io.Serializable;
+import java.util.Date;
+
+/* A recorded instance of the training data */
+
+public class DataInstance implements Serializable {
+
+	private static final long serialVersionUID = -1L;
+	public String label;
+	public Date date;
+	public float[] measurements;
+	
+	public String toCSVRow() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(',');
+		/*Capture Measurements corresponding to a single Unit label*/
+		for (int i = 0; i < measurements.length; i++) {
+			sb.append(measurements[i]);
+			sb.append(',');
+		}
+		sb.append(label);
+		return sb.toString();
+	}
+}
